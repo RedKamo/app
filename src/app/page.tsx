6bd6e382b5695823999ea62ac87352ca2aad0a6d
Camilo Suarez <camiloandressuarez1@gmail.com>
2023-08-02 17:32:37 -0500

    update file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1 className='text-4xl p-4 bg-blue-600'>Testing Next</h1>
+      <h1 className='text-4xl p-4 bg-blue-600'>Testing Next app</h1>
     </main>
   )
 }
